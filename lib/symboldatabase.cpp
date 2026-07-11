c9cb62fc1e6026533245e53f16f5011cb14f5cf4
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2026-07-11 14:30:26 +0200

    Fix #14897 fuzzing crash (null-pointer-use) in SymbolDatabase::createSymbolDatabaseSetScopePointers() (#8703)

--- lib/symboldatabase.cpp
+++ lib/symboldatabase.cpp
@@ -138,8 +138,14 @@ const Token* SymbolDatabase::isEnumDefinition(const Token* tok)
     if (tok->str() == "{")
         return tok;
     tok = tok->next(); // skip ':'
-    while (Token::Match(tok, "%name%|::"))
+    bool hasType = false;
+    while (Token::Match(tok, "%name%|::")) {
+        if (tok->isName())
+            hasType = true;
         tok = tok->next();
+    }
+    if (!hasType)
+        throw InternalError(tok, "SymbolDatabase bailout; invalid enum", InternalError::SYNTAX);
     return Token::simpleMatch(tok, "{") ? tok : nullptr;
 }
 
